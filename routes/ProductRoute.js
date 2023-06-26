19d8e2332da84e7e965b6ec091f73e12dcc9c300
Alifianf <alifianfathoroni2402@gmail.com>
2023-06-26 20:18:08 +0700

    add comment

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -9,10 +9,19 @@ import {
 
 const router = express.Router();
 
+// READ - GET
 router.get('/products', getProducts);
+
+// READ - GET by ID
 router.get('/products/:id', getProductById);
+
+// CREATE - POST
 router.post('/products', saveProduct);
+
+// UPDATE - PATCH
 router.patch('/products/:id', updateProduct);
+
+// DELETE - DELETE
 router.delete('/products/:id', deleteProduct);
 
 export default router;
